19907e691a40927cfe361604ad80b607ac271f36
Jose Alfonso Suárez Moreno <josealfonso.suarez@optimaonline.es>
2019-05-22 16:16:28 +0200

    Archivo de rutas y ruta por defecto.

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,10 @@
 
 // El snippet mateapp crea la estructura necesaria para empezar
 import 'package:flutter/material.dart';
+import 'package:componentes/src/routes/routes.dart';
+import 'package:componentes/src/pages/alert_page.dart';
 
 // import 'package:componentes/src/pages/home_temp.dart';
-import 'package:componentes/src/pages/home_page.dart';
-import 'package:componentes/src/pages/alert_page.dart';
-import 'package:componentes/src/pages/avatar_page.dart';
  
 void main() => runApp(MyApp());
  
@@ -18,10 +17,13 @@ class MyApp extends StatelessWidget {
       title: 'Componentes App',
       //home: HomePage(),
       initialRoute: '/',
-      routes: <String, WidgetBuilder>{
-        '/'       : ( BuildContext context ) => HomePage(),
-        'alert'   : ( BuildContext context ) => AlertPage(),
-        'avatar'  : ( BuildContext context ) => AvatarPage(),
+      routes: getApplicationsRoutes(),
+      onGenerateRoute: ( settings ) {
+
+        return MaterialPageRoute(
+          builder: ( BuildContext context) => AlertPage()
+        );
+
       },
       );
   }
---
+++ lib/src/routes/routes.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+
+import 'package:componentes/src/pages/home_page.dart';
+import 'package:componentes/src/pages/alert_page.dart';
+import 'package:componentes/src/pages/avatar_page.dart';
+
+Map<String, WidgetBuilder> getApplicationsRoutes() {
+
+  return <String, WidgetBuilder> {
+    '/'       : ( BuildContext context ) => HomePage(),
+    'alert'   : ( BuildContext context ) => AlertPage(),
+    'avatar'  : ( BuildContext context ) => AvatarPage(),
+  };
+
+}
