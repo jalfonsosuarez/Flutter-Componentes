b30707b1e8989fc0b70bbd8ad61b290c5c0ea10f
Jose Alfonso Suárez Moreno <josealfonso.suarez@optimaonline.es>
2019-05-22 15:39:41 +0200

    Funcion para obtener los iconos por nombre.

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:componentes/src/providers/menu_provider.dart';
+import 'package:componentes/src/utils/icono_string_util.dart';
 
 
 class HomePage extends StatelessWidget {
@@ -38,7 +39,7 @@ class HomePage extends StatelessWidget {
     data.forEach( (opt) {
       final widgetTemp = ListTile(
         title: Text( opt['texto'] ),
-        leading: Icon( Icons.account_circle, color: Colors.blue  ),
+        leading: getIcon( opt['icon'] ),
         trailing: Icon( Icons.keyboard_arrow_right, color: Colors.blue ),
         onTap: () {
 
@@ -46,11 +47,11 @@ class HomePage extends StatelessWidget {
         )
 
         opciones..add( widgetTemp )
-                ..add( Divider() )
+                ..add( Divider() );
     });
 
     return opciones;
-    
+
   }
 
   
