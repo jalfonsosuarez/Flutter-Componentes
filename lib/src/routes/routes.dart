61e09a45b94162bb56f9bdf583bbb36c10fe0d43
Jose Alfonso Suárez Moreno <josealfonso.suarez@optimaonline.es>
2019-05-22 16:35:10 +0200

    Primer tipo de tarjeta con sus botones.

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:componentes/src/pages/home_page.dart';
 import 'package:componentes/src/pages/alert_page.dart';
 import 'package:componentes/src/pages/avatar_page.dart';
+import 'package:componentes/src/pages/card_page.dart';
+
 
 Map<String, WidgetBuilder> getApplicationsRoutes() {
 
@@ -11,6 +13,7 @@ Map<String, WidgetBuilder> getApplicationsRoutes() {
     '/'       : ( BuildContext context ) => HomePage(),
     'alert'   : ( BuildContext context ) => AlertPage(),
     'avatar'  : ( BuildContext context ) => AvatarPage(),
+    'card'    : ( BuildContext context ) => CardPage(),
   };
 
 }
