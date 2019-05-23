1455d07b9269dc5f6f6735a8e734edef62f87f8f
Jose Alfonso Suárez Moreno <josealfonso.suarez@optimaonline.es>
2019-05-23 12:17:49 +0200

    InputText.

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -6,6 +6,7 @@ import 'package:componentes/src/pages/alert_page.dart';
 import 'package:componentes/src/pages/avatar_page.dart';
 import 'package:componentes/src/pages/card_page.dart';
 import 'package:componentes/src/pages/animated_container.dart';
+import 'package:componentes/src/pages/input_page.dart';
 
 
 Map<String, WidgetBuilder> getApplicationsRoutes() {
@@ -15,6 +16,7 @@ Map<String, WidgetBuilder> getApplicationsRoutes() {
     'alert'             : ( BuildContext context ) => AlertPage(),
     'avatar'            : ( BuildContext context ) => AvatarPage(),
     'card'              : ( BuildContext context ) => CardPage(),
+    'inputs'            : ( BuildContext context ) => InputPage(),
     'animatedContainer' : ( BuildContext context ) => AnimatedContainerPage(),
   };
 
