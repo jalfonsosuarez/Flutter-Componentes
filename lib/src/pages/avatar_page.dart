5596a15c785d419f7341c2efa515b10ad645e444
Jose Alfonso Suárez Moreno <josealfonso.suarez@optimaonline.es>
2019-05-23 11:18:18 +0200

    Avatars.

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -10,7 +10,32 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text( 'Avatar Page'),
+        actions: <Widget>[
+
+          Container(
+            padding: EdgeInsets.all( 5.0 ),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUXKNP-Vc0HdevOzxGtijAfEng445U1XHB4FzVsdnVhloVKUZwWw'),
+              radius: 20.0,
+            ),
+          ),
+
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Text( 'SL' ),
+              backgroundColor: Colors.brown,
+            ),
+          )
+        ],
       ),
+      body: Center(
+        child: FadeInImage(
+          image: NetworkImage('https://media.wired.com/photos/5be9d68a5d7c6a7b81d79e25/master/pass/StanLee-610719480.jpg'),
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          fadeInDuration: Duration( milliseconds: 200 ),
+        )
+      ) ,
     );
 
   }
