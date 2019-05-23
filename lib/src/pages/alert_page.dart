10b88b25af819aee3c0d0249b149226d0ece95dd
Jose Alfonso Suárez Moreno <josealfonso.suarez@optimaonline.es>
2019-05-23 11:04:29 +0200

    AlertDialog.

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -11,12 +11,59 @@ class AlertPage extends StatelessWidget {
       appBar: AppBar(
         title: Text( 'Alert Page'),
       ),
+
+      body: Center(
+        child: RaisedButton(
+          child: Text( 'Mostrar alerta'),
+          color: Colors.blue,
+          textColor: Colors.white,
+          shape: StadiumBorder(),
+          onPressed: () => _mostrarAlerta( context ),
+        ),
+      ),
+
       floatingActionButton: FloatingActionButton(
         child: Icon( Icons.add_location),
         onPressed: () {
           Navigator.pop(context);
         },
       ),
+    );
+
+  }
+
+  void _mostrarAlerta( BuildContext context ) {
+
+    showDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: ( context ) {
+
+        return AlertDialog(
+          shape: RoundedRectangleBorder( borderRadius: BorderRadius.circular( 20.0 ) ),
+          title: Text( 'Título' ),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text( "Contenido de la caja de la alerta"),
+              FlutterLogo( size: 100 ),
+            ],
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text( 'Cancelar' ),
+              onPressed: ()=> Navigator.of(context).pop(),
+            ),
+            FlatButton(
+              child: Text( 'Aceptar' ),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+
+      }
     );
 
   }
